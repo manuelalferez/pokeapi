33d0a78a0788a1c7148088888364cdb5e25e3b81
pmascaraque <ssm.pablo@gmail.com>
2021-05-10 16:31:50 +0200

    fetching pokedata successfully

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import fetchAll from "./utils/fetchAll.js";
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
+  const [pokeData, setPokeData] = useState(null);
+
+  const getPokemon = async () => {
+    const request = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+    const json = await request.json();
+    setPokeData(json);
+  };
+
   return (
-    <div>{fetchAll()}</div>
-  )
-}
+    <div>
+      <button
+        onClick={() => {
+          getPokemon();
+        }}
+      >
+        Fetch
+      </button>
+      <img src={pokeData ? pokeData.sprites.front_default : ""} alt=""/>
+    </div>
+  );
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<App />, rootElement);

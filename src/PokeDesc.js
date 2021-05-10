24a25b3c9f1b5b7e596411bedf4a175dcf122ff8
pmascaraque <ssm.pablo@gmail.com>
2021-05-10 14:23:17 +0200

    removed all

--- src/PokeDesc.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function PokeDesc({pokeDesc}) {
-    return (
-        <div>
-        <p>{JSON.stringify(pokeDesc)}</p>
-        </div>
-      );
-    }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+
+const App = () => {
+  return (
+    <div>Yello</div>
+  )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);

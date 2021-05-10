68d059f265924a68ce8dfe2a26ea8e8e9f7cb801
pmascaraque <ssm.pablo@gmail.com>
2021-05-10 16:54:02 +0200

    can choose pokemon now

--- src/index.js
+++ src/index.js
@@ -3,23 +3,37 @@ import ReactDOM from "react-dom";
 
 const App = () => {
   const [pokeData, setPokeData] = useState(null);
+  const [pokeNum, setPokeNum] = useState(1);
 
-  const getPokemon = async () => {
-    const request = await fetch("https://pokeapi.co/api/v2/pokemon/1");
+  const getPokemon = async (num) => {
+    const request = await fetch("https://pokeapi.co/api/v2/pokemon/" + num);
     const json = await request.json();
     setPokeData(json);
   };
 
+  let currentNum = pokeNum;
+
   return (
     <div>
+      <img src={pokeData ? pokeData.sprites.front_default : ""} alt="" />
       <button
         onClick={() => {
-          getPokemon();
+          currentNum--;
+          getPokemon(currentNum);
+          setPokeNum(currentNum);
         }}
       >
-        Fetch
+        Previous
       </button>
-      <img src={pokeData ? pokeData.sprites.front_default : ""} alt=""/>
+      <button
+        onClick={() => {
+          currentNum++;
+          getPokemon(currentNum)
+          setPokeNum(currentNum)
+        }}
+      >
+        Next
+      </button>
     </div>
   );
 };

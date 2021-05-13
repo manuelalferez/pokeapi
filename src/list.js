2ae3f969423248c173ef6809cee69b8c2520b651
pmascaraque <ssm.pablo@gmail.com>
2021-05-13 18:17:00 +0200

    pseudocode for list page

--- src/list.js
+++ src/list.js
@@ -1,9 +1,23 @@
 import "./styles/list.css";
 
 export default function List() {
+
+    const fetchList () => {
+        //fetch and create array with name and sprite of each pokemon,
+        // return name and sprite
+        //store last pokemon fetched number
+    }
+
+    const pokemon () => {
+        //create div with name and sprite image
+    }
+
+    const mapList () => {
+        //map through fetchList array with pokemon()
+    }
   return (
     <div className="container">
-            asdasd
+            
     </div>
   );
 };
